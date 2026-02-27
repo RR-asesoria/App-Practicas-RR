b65edb03c86f6fda5f8e3d1359343987ec0f503d
sergiogargoles <sergio.gargoles@educa.madrid.org>
2026-02-27 11:26:00 +0100

    Creada la estructura del proyecto y key de la bbdd

---
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/AppgestoriarrApplication.java
@@ -0,0 +1,13 @@
+package org.gestoriarr.appgestoriarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgestoriarrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgestoriarrApplication.class, args);
+	}
+
+}
---
+++ BackEnd/appgestoriarr/src/test/java/org/gestoriarr/appgestoriarr/AppgestoriarrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gestoriarr.appgestoriarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppgestoriarrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
