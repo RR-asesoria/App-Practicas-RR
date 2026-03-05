81246ddbd75961bb654156649f30e9132f01477e
jcaceresforteza <145029840+jcaceresforteza@users.noreply.github.com>
2026-03-05 10:50:33 +0100

    ClienteApp modelo

--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/model/ClienteApp.java
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/model/ClienteApp.java
@@ -1,4 +1,40 @@
 package org.gestoriarr.appgestoriarr.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+
 public class ClienteApp {
+
+
+    private String nombre;
+    private String nifCif;
+    private Date fechaNacimiento;
+    private String referencia;
+    private String casilla505anterior;
+    private String numerosCC;
+    private boolean datosFiscalesDescargados;
+    private String importe;
+    private TipoFacturado tipoFacturado;
+    private RecogidaDatos recogidaDatos;
+    private boolean excelDatosElaboracion;
+    private Borrador borrador;
+    private Presentada presentada;
+    private String cobrado;
+    private TipoCliente tipoCliente;
+    private EstadoCliente estadoCliente;
+    private String casilla505Actual;
+
+
+
 }
