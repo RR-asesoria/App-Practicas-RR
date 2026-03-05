e4f3301514fbe94ef0bfa6bf7a47301aabdfc49c
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-03-05 11:22:41 +0100

    Agregando anotaciones de lombok

--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/model/Usuario.java
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/model/Usuario.java
@@ -1,5 +1,13 @@
 package org.gestoriarr.appgestoriarr.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Usuario {
     private String nombre;
     private String clave;
--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/repository/UsuarioRepo.java
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/repository/UsuarioRepo.java
@@ -1,4 +1,17 @@
 package org.gestoriarr.appgestoriarr.repository;
 
 public class UsuarioRepo {
+
+    //TODO CRUD
+
+    //crearUsuario
+
+    //borrarUsuario
+
+    //actualizarUsuario
+
+    //BorrarUsuario
+
+
+
 }
