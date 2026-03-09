9be40d37d261f29a29dad4556a85fc665fdad32d
Sanmiguel2420 <128133617+Sanmiguel2420@users.noreply.github.com>
2026-03-09 09:59:07 +0100

    Modificacion de ClienteAppHistoricoController

--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/controller/ClienteAppHistoricoController.java
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/controller/ClienteAppHistoricoController.java
@@ -1,6 +1,14 @@
 package org.gestoriarr.appgestoriarr.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.gestoriarr.appgestoriarr.model.ClienteApp;
 import org.gestoriarr.appgestoriarr.model.ClienteAppHistorico;
 import org.gestoriarr.appgestoriarr.repository.FiltroCliente;
 import org.gestoriarr.appgestoriarr.service.ClienteAppHistoricoService;
@@ -21,6 +29,11 @@ public class ClienteAppHistoricoController {
         this.clienteHistoricoService = clienteHistoricoService;
     }
 
+    @Operation(summary = "Obtener cliente", description = "Obtener un cliente mediante su Nif/Cif")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cliente Encontrado en el historico", content = @Content(schema = @Schema(implementation = ClienteAppHistorico.class))),
+            @ApiResponse(responseCode = "404", description = "Cliente no Encontrado en el historico", content = @Content)
+    })
     @GetMapping("/{nifCif}")
     public ResponseEntity<ClienteAppHistorico> ObtenerCliente(@PathVariable String nifCif) {
         try {
@@ -31,13 +44,23 @@ public class ClienteAppHistoricoController {
         }
     }
 
+    @Operation(summary = "Obtener todos los clientes", description = "Devuelve la lista completa de clientes en el historico")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Historico de clientes", content =  @Content(schema = @Schema(implementation = ClienteAppHistorico.class))),
+            @ApiResponse(responseCode = "404", description = "Historico de clientes no encontrado", content = @Content)
+    })
     @GetMapping
     public List<ClienteAppHistorico> ObtenerClientes() {
         return clienteHistoricoService.obtenerTodosClientes();
     }
 
+    @Operation(summary = "Eliminar cliente historico", description = "Elimina un cliente del historico por su NIF/CIF")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cliente eliminado correctamente del historico"),
+            @ApiResponse(responseCode = "404", description = "Cliente no encontrado", content = @Content)
+    })
     @DeleteMapping("/{nifCif}")
-    public ResponseEntity<String> eliminarCliente(@PathVariable String nifCif) {
+    public ResponseEntity<String> eliminarCliente(@Parameter(description = "NIF/CIF del cliente a eliminar", required = true) @PathVariable String nifCif) {
         try {
             clienteHistoricoService.eliminarCliente(nifCif);
             return ResponseEntity.ok("Cliente eliminado correctamente");
@@ -46,14 +69,31 @@ public class ClienteAppHistoricoController {
         }
     }
 
+    @Operation(summary = "Buscar clientes", description = "Busca clientes en el historico según filtros exactos seleccionados")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Clientes encontrados", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ClienteAppHistorico.class)))),
+            @ApiResponse(responseCode = "404",description = "Clientes no encontrados", content = @Content)
+    })
     @PostMapping("/buscar")
-    public List<ClienteAppHistorico> buscarPorFiltros(@RequestBody Map<String, FiltroCliente> filtros) {
+    public List<ClienteAppHistorico> buscarPorFiltros(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Filtros de búsqueda exacta por atributos (tipoCliente, nifCif, estadoCliente...)",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = ClienteAppHistorico.class))
+            )
+            @RequestBody Map<String, FiltroCliente> filtros) {
         return clienteHistoricoService.buscarPorFiltros(filtros);
     }
 
+    @Operation(summary = "Buscar clientes en el historico por nombre", description = "Busca clientes por coincidencia parcial de nombre")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Clientes encontrados", content = @Content(schema = @Schema(implementation = ClienteAppHistorico.class)))
+    })
     @GetMapping("/buscar/nombre")
-    public List<ClienteAppHistorico> buscarPorNombre(@RequestParam String nombre) {
+    public List<ClienteAppHistorico> buscarPorNombre(@Parameter(description = "Nombre a buscar", required = true)@RequestParam String nombre) {
         return clienteHistoricoService.buscarPorNombre(nombre);
     }
+
+    /*Puede que crear metodos para aniadir usuarios y usuario*/
 
 }
