49481cb6a0169441069da418eab100b618502af8
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-07 07:34:34 +0200

    Quitando el paquete de excepciones

--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package org.gestoriarr.appgestoriarr.exception;
-
-public class UnauthorizedException extends RuntimeException{
-	
-	public UnauthorizedException(String message) {
-		super(message);
-	}
-	
-}
--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.gestoriarr.appgestoriarr.exception;
-
-public class UserNotFoundException extends RuntimeException{
-
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-	
-}
