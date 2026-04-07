4edad6e9d4c0fa8473b528012dab6c34b4e2d7dc
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-07 07:28:23 +0200

    Creando endpoint para el logout

--- BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/controller/AuthController.java
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/controller/AuthController.java
@@ -2,35 +2,37 @@ package org.gestoriarr.appgestoriarr.controller;
 //TEST DE CONEXIÓN
 
 
-import org.gestoriarr.appgestoriarr.dto.UsuarioCreacionDTO;
-import org.gestoriarr.appgestoriarr.service.UsuarioService;
+import com.google.firebase.auth.FirebaseAuth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AdminController {
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
 
-    private final UsuarioService service;
 
-    public AdminController(UsuarioService service){
-        this.service = service;
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(Authentication authentication) {
+
+        try {
+
+            String uid = (String) authentication.getPrincipal();
+
+            FirebaseAuth.getInstance().revokeRefreshTokens(uid);
+
+            return ResponseEntity.ok("Logout correcto");
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al hacer logout");
+        }
+
     }
-
-    @GetMapping("/conexion")
-    public ResponseEntity<String> conexionApiCliente(){
-        return ResponseEntity.ok("Conexión con API");
-    }
-
-    @PostMapping("/usuario")
-    public ResponseEntity<String> crearUsuario(UsuarioCreacionDTO dto) throws Exception {
-
-            service.crearUsuario(dto);
-            return ResponseEntity.ok("Usuario creado.");
-
-    }
-
-    //COMENTARIO RANDOM
 
 }
