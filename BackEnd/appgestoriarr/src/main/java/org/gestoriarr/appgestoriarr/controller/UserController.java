2dc6ef40ec24be2e81190eca4e6b6af3f8fea56f
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-08 11:13:40 +0200

    Agregando endpoints

---
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/controller/UserController.java
@@ -0,0 +1,171 @@
+package org.gestoriarr.appgestoriarr.controller;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.gestoriarr.appgestoriarr.dto.CambioPasswordDTO;
+import org.gestoriarr.appgestoriarr.dto.UsuarioActualizarDTO;
+import org.gestoriarr.appgestoriarr.dto.UsuarioCreacionDTO;
+import org.gestoriarr.appgestoriarr.dto.UsuarioRespuestaDTO;
+import org.gestoriarr.appgestoriarr.service.UsuarioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UsuarioService service;
+
+    //CREATE
+    @PostMapping("/crearusuario")
+    public ResponseEntity<String> crearUsuario(@RequestBody UsuarioCreacionDTO dto){
+
+        try {
+            service.crearUsuario(dto);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body("Usuario creado");
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+
+
+    }
+
+    //READ
+    @GetMapping("/buscarporid/{uid}")
+    public ResponseEntity<UsuarioRespuestaDTO>encontrarPorId(@PathVariable String uid){
+
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(service.encontrarPorId(uid));
+
+        } catch (Exception e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+
+
+    }
+
+    @GetMapping("/buscarporid/{email}")
+    public ResponseEntity<UsuarioRespuestaDTO> encontrarPorEmail(@PathVariable String email){
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(service.encontrarPorEmail(email));
+
+        } catch (Exception e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @GetMapping("/buscarporid/{nombre}")
+    public ResponseEntity<UsuarioRespuestaDTO> encontrarPorNombre(@PathVariable String nombre){
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.FOUND)
+                    .body(service.encontrarPorNombre(nombre));
+
+        } catch (Exception e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @GetMapping("/todoslosusuarios")
+    public  ResponseEntity<List<UsuarioRespuestaDTO>> obtenerTodos(){
+
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(service.obtenerTodos());
+
+        } catch (Exception e) {
+
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .build();
+        }
+
+
+    }
+
+    //UPDATE
+    @PutMapping("/actualizarusuario/{uid}")
+    public ResponseEntity<String> actualizar(@PathVariable String uid, @RequestBody UsuarioActualizarDTO dto){
+
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(service.actualizar(uid, dto));
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+
+
+    }
+
+    @PutMapping("/actualizarpassword/{uid}")
+    public ResponseEntity<String> cambiarPassword(@PathVariable String uid,@RequestBody CambioPasswordDTO dto){
+
+        try {
+
+            service.cambiarPassword(uid, dto);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body("Contraseña actualizada");
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+
+
+    }
+
+    //DELETE
+    @DeleteMapping("/eliminarusuario/{uid}")
+    public ResponseEntity<String> eliminarUsuario(String uid){
+
+        try {
+
+            service.eliminarUsuario(uid);
+
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .body("Usuario eliminado");
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+
+
+    }
+}
