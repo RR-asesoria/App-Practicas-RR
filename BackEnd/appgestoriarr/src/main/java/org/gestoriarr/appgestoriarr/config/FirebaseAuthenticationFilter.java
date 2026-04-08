d5ea47f7a668943a422859d98879836b1809f31c
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-08 12:00:29 +0200

    Agregando el filtro

---
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/config/FirebaseAuthenticationFilter.java
@@ -0,0 +1,314 @@
+package com.firebaselogin.pruebafirebaselogin.config;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.firebaselogin.pruebafirebaselogin.model.Usuario;
+import com.firebaselogin.pruebafirebaselogin.service.UsuarioService;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FirebaseAuthenticationFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, 
+									HttpServletResponse response, 
+									FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String header = request.getHeader("Authorization");
+	
+		
+		if(header == null || !header.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+			
+		String token = header.substring(7);
+			
+		try {
+				
+				FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token,true);
+				
+				String uid = decodedToken.getUid();
+				
+				if(SecurityContextHolder.getContext().getAuthentication() == null) {
+					
+					Usuario usuario = usuarioService.obtenerUsuarioInterno(uid);
+					
+					List<GrantedAuthority> authiAuthorities = 
+							List.of(new SimpleGrantedAuthority("ROLE_"+usuario.getRole()));
+					
+					
+					UsernamePasswordAuthenticationToken authentication = 
+							new UsernamePasswordAuthenticationToken(uid, null, authiAuthorities);
+					
+					authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					
+					SecurityContextHolder.getContext().setAuthentication(authentication);
+					
+				}
+				
+		}catch (FirebaseAuthException a) {
+			// TODO Auto-generated catch block
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Token invalido");
+			return;
+		} 
+		catch (Exception e) {
+				// TODO Auto-generated catch block
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			response.getWriter().write("Usuario no autorizado");
+			return;
+			}
+			
+			filterChain.doFilter(request, response);
+			
+		};
+		
+		
+		
+		
+	}
+
+
+/*
+ * 
+ * VERSIÓN FUNCIONAL 1 DEL FILTRO
+ * 
+ * @Autowired
+	private UsuarioService usuarioService;
+ * 
+ * 
+ * @Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		//TODO BLOQUE PARA VER TODAS LAS PETICIONES ENTRANTES
+		Enumeration<String> headers = request.getHeaderNames();
+		
+
+		while (headers.hasMoreElements()) {
+		    String h = headers.nextElement();
+		    System.out.println("HEADER: " + h + " = " + request.getHeader(h));
+		}
+		
+		
+		String header = request.getHeader("Authorization");
+		
+		
+		//TODO VEMOS SI EL TOKEN RECIBIDO ES NULO
+		System.out.println("Hola desde doFilterInternal. El header es nulo?"+ (header == null));
+		
+		
+		
+		if(header == null || !header.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+			
+		String token = header.substring(7);
+		
+		
+			
+		try {
+				
+				FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+				
+				String uid = decodedToken.getUid();
+				
+				
+				Usuario usuario = usuarioService.obtenerUsuario(uid);
+				
+				List<GrantedAuthority> authiAuthorities = 
+						List.of(new SimpleGrantedAuthority("ROLE_"+usuario.getRole()));
+				
+				UsernamePasswordAuthenticationToken authentication = 
+						new UsernamePasswordAuthenticationToken(uid, null, authiAuthorities);
+				
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				
+				
+		} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			filterChain.doFilter(request, response);
+			
+		};
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+
+
+/*
+ * 
+ * SEGUNDA VERSIÓN FUNCIONAL CON CLAIMS
+ * 
+ * 
+ * @Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		//TODO BLOQUE PARA VER TODAS LAS PETICIONES ENTRANTES
+		Enumeration<String> headers = request.getHeaderNames();
+		
+
+		while (headers.hasMoreElements()) {
+		    String h = headers.nextElement();
+		    System.out.println("HEADER: " + h + " = " + request.getHeader(h));
+		}
+		
+		
+		String header = request.getHeader("Authorization");
+		
+		
+		//TODO VEMOS SI EL TOKEN RECIBIDO ES NULO
+		System.out.println("Hola desde doFilterInternal. El header es nulo?"+ (header == null));
+		
+		
+		
+		if(header == null || !header.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+			
+		String token = header.substring(7);
+		
+		
+			
+		try {
+				
+				FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+				
+				String uid = decodedToken.getUid();
+				
+				
+				
+				String role = (String) decodedToken.getClaims().get("role");
+				
+				if(role == null) {
+					throw new RuntimeException("El usuario no tiene un rol asignado");
+				}
+				
+				List<GrantedAuthority> authiAuthorities = 
+						List.of(new SimpleGrantedAuthority("ROLE_"+role));
+				
+				UsernamePasswordAuthenticationToken authentication = 
+						new UsernamePasswordAuthenticationToken(uid, null, authiAuthorities);
+				
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				
+				
+		} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			filterChain.doFilter(request, response);
+			
+		};
+ * 
+ * 
+ * @Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		//TODO BLOQUE PARA VER TODAS LAS PETICIONES ENTRANTES
+		Enumeration<String> headers = request.getHeaderNames();
+		
+
+		while (headers.hasMoreElements()) {
+		    String h = headers.nextElement();
+		    System.out.println("HEADER: " + h + " = " + request.getHeader(h));
+		}
+		
+		
+		String header = request.getHeader("Authorization");
+		
+		
+		//TODO VEMOS SI EL TOKEN RECIBIDO ES NULO
+		System.out.println("Hola desde doFilterInternal. El header es nulo?"+ (header == null));
+		
+		
+		
+		if(header == null || !header.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+			
+		String token = header.substring(7);
+			
+		try {
+				
+				FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+				
+				String uid = decodedToken.getUid();
+				
+				Usuario usuario = usuarioService.obtenerUsuarioInterno(uid);
+				
+				//PARA VER AL USUARIO AUTENTICADO ACTUALMENTE
+				String context = (String) SecurityContextHolder
+					.getContext().getAuthentication()
+					.getPrincipal();
+				
+				List<GrantedAuthority> authiAuthorities = 
+						List.of(new SimpleGrantedAuthority("ROLE_"+usuario.getRole()));
+				
+				UsernamePasswordAuthenticationToken authentication = 
+						new UsernamePasswordAuthenticationToken(uid, null, authiAuthorities);
+				
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				
+		} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			filterChain.doFilter(request, response);
+			
+		};
+		
+		
+		
+		
+	}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+	
+	
+
+
