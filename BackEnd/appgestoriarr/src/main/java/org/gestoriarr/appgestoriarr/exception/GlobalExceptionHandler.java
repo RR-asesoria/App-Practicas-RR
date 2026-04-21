1cedcbb55e9c1c2093624fbedcd27acdbc88b2ba
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-04-21 17:29:54 +0200

    Creando el manejador de excepciones y una excepción personalizada para evitar el exception generico

---
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/exception/GlobalExceptionHandler.java
@@ -0,0 +1,88 @@
+package org.gestoriarr.appgestoriarr.exception;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuthException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errors);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handlerIllegalStateException(IllegalStateException e){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handlerUserNotFoundException(UserNotFoundException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handlerIllegalArgumentException(IllegalArgumentException e){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(FirebaseAuthException.class)
+    public ResponseEntity<?> handlerFirebaseAuthException(FirebaseAuthException e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(FirebaseException.class)
+    public ResponseEntity<?> handlerFirebaseException(FirebaseException e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handlerRuntimeException(RuntimeException e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(AssertionError.class)
+    public ResponseEntity<?> handlerAssertionError(AssertionError e){
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.getMessage());
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ BackEnd/appgestoriarr/src/main/java/org/gestoriarr/appgestoriarr/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.gestoriarr.appgestoriarr.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
